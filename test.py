5d7c300d0423bd01c7fd54a9f824a5101c5b2e64
crossrepublic <71635944+ziminl@users.noreply.github.com>
2023-06-29 18:26:49 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-ping pong
+
+
+
+import nextcord
+from nextcord.ext import commands
+
+
+token = ''
+
+
+
+intents = nextcord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents = intents, description='4test')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+bot.run(token)
+
+
